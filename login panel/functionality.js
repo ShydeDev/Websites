90a8d36dad99346ec98fd780263462213c75dbc9
Shyde <156099484+ShydeDev@users.noreply.github.com>
2024-05-17 23:15:39 +0200

    Add files via upload

---
+++ login panel/functionality.js
@@ -0,0 +1,5 @@
+function buttonClick() {
+    var username = document.getElementById("username-input")
+    var password = document.getElementById("password-input")
+    alert("Username: " + username.value + "\nPassword: " + password.value + "\nIt also didnt do a shit bc website literally does nothing")
+}
